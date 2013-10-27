f824d64b44ae967410df7404e0d63f0bc1cb047f
Brian P. Johnson <bjohnson@trulia.com>
2013-10-27 12:23:54 -0700

    Improving workflow by adding node server to root, and working so IOS and web app share same www dir

---
+++ server/app.js
@@ -0,0 +1,46 @@
+/**
+ * Server for Care-X
+ * @type {exports|*}
+ */
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+  , mongoose = require('mongoose')
+  , config = require('./config');
+/**
+ * Routing Modules
+ * @type {api|exports|*}
+ */
+var users = require('./routes/user')
+  , events = require('./routes/events')
+  , index = require('./routes/index');
+/**
+ * Connecting to the database
+ */
+mongoose.connect('mongodb://localhost/' + config.dbName);
+/**
+ * Setting up the server and middleware
+ * @type {*|exports}
+ */
+var app = express();
+app.use(express.bodyParser());
+app.set('port', config.port);
+app.set('views', __dirname + '/../www');
+app.set('view engine', 'ejs');
+app.engine('.html', require('ejs').__express);
+app.use(express.favicon());
+app.use(express.methodOverride());
+app.use(express.cookieParser(config.secret));
+app.use(express.cookieSession({secret: config.secret, cookie: {path: '/', httpOnly: false, maxAge: null}}));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, '/../www')));
+/**
+ * Configuring the routes as middleware
+ */
+app.use('/', index);
+app.use('/users', users);
+app.use('/events', events);
+http.createServer(app).listen(app.get('port'), function() {
+  console.log(config.service + ' listening on port ' + config.port);
+});
---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express')
+  , api = express();
+/**
+ * loading the main page
+ */
+var init = function(req, res, next) {
+  console.log('init');
+  return next();
+};
+var index = function(req, res, next) {
+  res.render('index.html', {});
+};
+api.all("/", init);
+api.get("/", index);
+module.exports = api;
